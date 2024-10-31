42f02bd442840d70d944a61e14d90efa9fa8ac48
Ramiz Hida <ramizhida00@gmail.com>
2024-10-31 13:57:42 +0100

    set up template engine, add views and display mock data in index view

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const path = require('node:path');
 
 // Import Routers
 const indexRouter = require('./routes/indexRouter');
 const newMessageRouter = require('./routes/newMessageRouter');
+
+// Set view engine
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
 // Define Routes
 app.use('/', indexRouter);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,27 @@ const { Router } = require('express');
 
 const indexRouter = Router();
 
+// Sample messages mock data
+const messages = [
+  {
+    text: 'First Message',
+    user: 'Stolen Dessert',
+    time: new Date(),
+  },
+  {
+    text: 'Second Message',
+    user: 'Johnny Bravo',
+    time: new Date(),
+  },
+  {
+    text: 'Final Message',
+    user: 'Scooby Doo',
+    time: new Date(),
+  },
+];
+
 indexRouter.get('/', (req, res) => {
-  res.send('Index Router');
+  res.render('index', { title: 'Home Page', messages: messages });
 });
 
 module.exports = indexRouter;
